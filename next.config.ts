7eb6f61b85a988588f6cad9900d22e78a11c28c8
D3Portillo <d3portillo@gmail.com>
2025-06-02 07:14:42 +0200

    fix(next.config.ts): mute pino-pretty warn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { webpack }) => {
+    config.plugins = config.plugins || []
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pino-pretty$/,
+      })
+    )
+
+    return config
+  },
 }
 
 export default nextConfig
